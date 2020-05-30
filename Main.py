a9e575f27658b710d0831d0a1404f5e9c0f8f4b5
satiriorn <satiriorn@gmail.com>
2020-05-30 13:14:42 +0300

    v 0.1

--- Main.py
+++ Main.py
@@ -1,14 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
-from telegram import InlineQueryResultArticle, InputMediaPhoto, ParseMode, InputTextMessageContent, \
-    InlineQueryResultCachedVoice, \
-    InlineQueryResultPhoto, InputMessageContent, InlineQueryResultCachedPhoto
-from telegram.utils.helpers import escape_markdown
-from uuid import uuid4
-import badge, re, sqlite3, start, help, weather, Evtuh, text
-from time import sleep
-from gtts import gTTS
-import pyttsx3
-from googletrans import Translator
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import badge, start, help, weather, Evtuh, text
 
 
 def main():
@@ -27,6 +18,6 @@ def main():
     dispatcher.add_handler(text_message_handler)
 
     updater.start_polling(clean=True)
-    
+
 if __name__ == '__main__':
     main()
