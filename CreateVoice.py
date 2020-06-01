64985861286870093f88c4544919f43898e2addd
satiriorn <satiriorn@gmail.com>
2020-06-01 23:47:31 +0300

    voice v1.0 without ukrainian

--- CreateVoice.py
+++ CreateVoice.py
@@ -4,21 +4,24 @@ from gtts_token.gtts_token import Token
 from langdetect import detect
 
 def _patch_faulty_function(self):
-    if self.token_key is not None:
-        return self.token_key
+    try:
+        if self.token_key is not None:
+            return self.token_key
 
-    timestamp = calendar.timegm(time.gmtime())
-    hours = int(math.floor(timestamp / 3600))
+        timestamp = calendar.timegm(time.gmtime())
+        hours = int(math.floor(timestamp / 3600))
 
-    results = requests.get("https://translate.google.com/")
-    tkk_expr = re.search("(tkk:*?'\d{2,}.\d{3,}')", results.text).group(1)
-    tkk = re.search("(\d{5,}.\d{6,})", tkk_expr).group(1)
+        results = requests.get("https://translate.google.com/")
+        tkk_expr = re.search("(tkk:*?'\d{2,}.\d{3,}')", results.text).group(1)
+        tkk = re.search("(\d{5,}.\d{6,})", tkk_expr).group(1)
 
-    a, b = tkk.split('.')
+        a, b = tkk.split('.')
 
-    result = str(hours) + "." + str(int(a) + int(b))
-    self.token_key = result
-    return result
+        result = str(hours) + "." + str(int(a) + int(b))
+        self.token_key = result
+        return result
+    except Exception:
+        print("0")
 
 Token._get_token_key = _patch_faulty_function
 
