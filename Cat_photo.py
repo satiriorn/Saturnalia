b526582ff242e1d21f5ef9694c985cfcc8eabe83
satiriorn <satiriorn@gmail.com>
2020-06-02 23:04:37 +0300

    send dog

--- Cat_photo.py
+++ Cat_photo.py
@@ -1,16 +1,37 @@
-import  os, random, badge
+import  os, random, badge, datetime, requests
+now = datetime.datetime.now()
 
-def Cat_photo(bot, update, status=True):
+
+def get_url():
+    contents = requests.get('http://aws.random.cat/meow').json()
+    url = contents['url']
+    return url
+
+
+def is_image(url):
+    allowed_extension = ['jpg', 'jpeg', 'png']
+    flag = False
+    for allowed_ext in allowed_extension:
+        if url.lower().endswith(allowed_ext):
+            flag = True
+            return flag
+    return flag
+
+def is_animation(url):
+    allowed_extension = ['mp4', 'gif']
+    flag = False
+    for allowed_ext in allowed_extension:
+        if url.lower().endswith(allowed_ext):
+            flag = True
+            return flag
+    return flag
+
+def Cat_photo(bot, update):
     try:
-        if status == True:
-            file = os.listdir('telegramCat')
-            r = random.randint(0, 207)
-            t = 'telegramCat/'
-            return bot.send_photo(update.message.chat_id, open(t + file[r], 'rb'))
-        else:
-            file = badge.httpCat
-            r = random.randint(0, len(file))
-            print(file[r])
-            return file[r]
+        url = get_url()
+        if is_image(url):
+            update.message.reply_photo(url)
+        elif is_animation(url):
+            update.message.reply_animation(url)
     except Exception:
         bot.send_message(update.message.chat_id, "Пока без котиков, только не плачь, умоляю")
