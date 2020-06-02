4a58c33cae4a079b292354c2fd02055e2785dbab
satiriorn <satiriorn@gmail.com>
2020-06-03 00:14:58 +0300

    cat and dog

--- DogAndCat.py
+++ DogAndCat.py
@@ -1,14 +1,16 @@
-import  os, random, badge, datetime, requests
+import  datetime, requests
+
 now = datetime.datetime.now()
 
+def get_url_cat():
+    contents = requests.get('http://aws.random.cat/meow').json()
+    url = contents['file']
+    return url
 
-def get_url():
-    try:
-        contents = requests.get('http://aws.random.cat/meow').json()
-        url = contents['file']
-        return url
-    e
-
+def get_url_dog():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
 
 def is_image(url):
     allowed_extension = ['jpg', 'jpeg', 'png']
@@ -28,9 +30,23 @@ def is_animation(url):
             return flag
     return flag
 
-def Cat_photo(bot, update):
-    url = get_url()
+def Photo(url, update):
     if is_image(url):
         update.message.reply_photo(url)
     elif is_animation(url):
         update.message.reply_animation(url)
+
+def Cat_photo(bot, update):
+    try:
+        url = get_url_cat()
+        Photo(url, update)
+    except Exception:
+        bot.send_message(update.message.chat_id, "Пока без котиков, только не плачь, умоляю")
+
+
+def Dog_photo(bot, update):
+    try:
+        url = get_url_dog()
+        Photo(url, update)
+    except Exception:
+        bot.send_message(update.message.chat_id, "Пока без собакеном, только не плачь, умоляю")
