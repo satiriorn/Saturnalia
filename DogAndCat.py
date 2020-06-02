c5a395898033b70af83155dd96e71590e307bbaa
satiriorn <satiriorn@gmail.com>
2020-06-02 23:57:52 +0300

    send cat

--- DogAndCat.py
+++ DogAndCat.py
@@ -3,9 +3,11 @@ now = datetime.datetime.now()
 
 
 def get_url():
-    contents = requests.get('http://aws.random.cat/meow').json()
-    url = contents['url']
-    return url
+    try:
+        contents = requests.get('http://aws.random.cat/meow').json()
+        url = contents['file']
+        return url
+    e
 
 
 def is_image(url):
@@ -27,11 +29,8 @@ def is_animation(url):
     return flag
 
 def Cat_photo(bot, update):
-    try:
-        url = get_url()
-        if is_image(url):
-            update.message.reply_photo(url)
-        elif is_animation(url):
-            update.message.reply_animation(url)
-    except Exception:
-        bot.send_message(update.message.chat_id, "Пока без котиков, только не плачь, умоляю")
+    url = get_url()
+    if is_image(url):
+        update.message.reply_photo(url)
+    elif is_animation(url):
+        update.message.reply_animation(url)
