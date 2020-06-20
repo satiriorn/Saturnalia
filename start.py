3910114715692a02002d53ddbc902004899b5260
satiriorn <satiriorn@gmail.com>
2020-06-20 19:47:10 +0300

    Keyboard

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import Keyboard
 def start(bot, update):
     print(update)
     try:
@@ -15,7 +16,7 @@ def start(bot, update):
         print(results)"""
         if 'Rositsa Maneva' in first_name:
             bot.send_message(chat_id, """Слава создателю, мне написала сама """ + first_name)
-            bot.send_message(chat_id,
+            bot.send_message(chat_i d,
                              """Честно говоря, я не ожидал тебя здесь увидеть, но для меня большая честь видеть вас тут(Я поцеловал ваш телефон в микросхему, сейчас он улыбается, можете погладить его), а для вас у меня особая программа...УХАХАХАХАХА """)
 
         else:
@@ -32,6 +33,6 @@ def start(bot, update):
                          /Dog рандомные собакены для экстравертов
                          Пока что это всё, а то твои некомплектные мозги отстали от жизни и ты сдохнешь от этого
                          
-                         """)
+                         """,reply_markup=Keyboard.InitKeyboard())
     except Exception:
         bot.send_message(update.message.chat_id, 'Прости, но я мертв для тебя')
