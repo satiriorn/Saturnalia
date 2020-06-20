0f493b31cebccd0f59eea3f997e2c57fceb02d03
satiriorn <satiriorn@gmail.com>
2020-06-20 18:53:16 +0300

    Keyboard

--- Keyboard.py
+++ Keyboard.py
@@ -1,1 +1,11 @@
-SelectButton = []
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+NameButton = ["/Evtuh","/Weather","/Voice", "/ShevchenkoStyle", "/Cat", "/Dog", "/Help"]
+
+def InitKeyboard():
+    Button = []
+    for i in range(len(NameButton)):
+        Button =KeyboardButton(NameButton[i])
+    markup = ReplyKeyboardMarkup().row(Button[0],Button[1],Button[2])
