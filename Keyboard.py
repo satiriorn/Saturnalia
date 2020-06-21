762150835d3af11b5b112f61a417ca67eb503955
satiriorn <satiriorn@gmail.com>
2020-06-21 19:42:04 +0300

    Keyboard horizontal all button

--- Keyboard.py
+++ Keyboard.py
@@ -1,9 +1,8 @@
 import telegram
-
 NameButton = ["/Evtuh","/Weather","/Voice", "/ShevchenkoStyle", "/Cat", "/Dog", "/Help"]
 
 def InitKeyboard():
     Button = []
     for i in range(len(NameButton)):
-        Button = telegram.KeyboardButton(NameButton[i])
+        Button.append([telegram.KeyboardButton(NameButton[i])])
     return telegram.ReplyKeyboardMarkup(Button, resize_keyboard=True)
