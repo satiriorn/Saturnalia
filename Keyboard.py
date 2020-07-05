413772596c5fd8d86d6370905ca987bcad1274e8
satiriorn <satiriorn@gmail.com>
2020-07-05 19:38:55 +0300

    Keyboard v.2.0

--- Keyboard.py
+++ Keyboard.py
@@ -2,7 +2,16 @@ import telegram
 NameButton = ["/Evtuh","/Weather","/Voice", "/ShevchenkoStyle", "/Cat", "/Dog", "/Help"]
 
 def InitKeyboard():
+    LevelOne = []
+    LevelThree = []
     Button = []
     for i in range(len(NameButton)):
-        Button.append([telegram.KeyboardButton(NameButton[i])])
+        if i<3:
+            LevelOne.append(telegram.KeyboardButton(NameButton[i]))
+        elif i==3:
+            Button.append(LevelOne)
+            Button.append([telegram.KeyboardButton(NameButton[i])])
+        else:
+            LevelThree.append(telegram.KeyboardButton(NameButton[i]))
+    Button.append(LevelThree)
     return telegram.ReplyKeyboardMarkup(Button, resize_keyboard=True)
