9ca0a9d6d7db416a0d4556f3d7ad181eac1402cb
satiriorn <satiriorn@gmail.com>
2020-08-24 22:09:39 +0300

    Meme 2.0

--- Meme.py
+++ Meme.py
@@ -1,5 +1,8 @@
-import requests
+import requests, Keyboard
 
-def Get_meme(update):
-    c = requests.get('https://meme-api.herokuapp.com/gimme').json()
-    update.message.reply_photo(c['url'])
+def Get_meme(bot, update):
+    try:
+        c = requests.get('https://meme-api.herokuapp.com/gimme').json()
+        update.message.reply_photo(c['url'])
+    except Exception:
+        bot.send_message(update.message.chat_id, "Ти точно людина? Меми, не хочуть бути с тобою.", reply_markup=Keyboard.InitKeyboard())
