7f152c23868e9e479aea87fdcbb62def793001e0
satiriorn <satiriorn@gmail.com>
2020-08-25 16:40:31 +0300

    test keyboard

--- help.py
+++ help.py
@@ -1,4 +1,4 @@
 import Keyboard
 def help(bot, update):
     bot.send_message(update.message.chat_id, '\
-    Тобі потрібна допомога? Я дав тобі кнопки влади наді мною. Користуйся мною скільки захочеш, я весь твій. ', reply_markup = Keyboard.InitKeyboard())
+    Тобі потрібна допомога? Я дав тобі кнопки влади наді мною. Користуйся мною скільки захочеш, я весь твій. ',  Keyboard.InitKeyboard())
