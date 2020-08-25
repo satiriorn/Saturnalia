56457bac343aac44a1c845c1f555c352f238ebc1
satiriorn <satiriorn@gmail.com>
2020-08-25 14:24:58 +0300

    Refactoring

--- Evtuh.py
+++ Evtuh.py
@@ -1,8 +1,11 @@
 import badge, random, Keyboard
 
 def Evtuh(bot, update, status=True):
-    r = random.randint(0, len(badge.God)-1)
-    if status == True:
-        bot.send_message(update.message.chat_id, badge.God[r], reply_markup = Keyboard.InitKeyboard())
-    else:
-        return badge.God[r]
+    try:
+        r = random.randint(0, len(badge.God)-1)
+        if status == True:
+            bot.send_message(update.message.chat_id, badge.God[r], reply_markup = Keyboard.InitKeyboard())
+        else:
+            return badge.God[r]
+    except Exception:
+        bot.send_message(update.message.chat_id, "Ти не потрібен українському богу віршів.", reply_markup = Keyboard.InitKeyboard())
