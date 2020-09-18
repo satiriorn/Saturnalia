4ee3064e95a5f40871a25ad232385de3333ec091
satiriorn <satiriorn@gmail.com>
2020-09-18 22:53:30 +0300

    refactoring code

--- weather.py
+++ weather.py
@@ -1,10 +1,10 @@
-import requests
+import requests, os
 
 def weather(update, context):
     try:
         count = 0
         res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                           params={'q': 'Kharkiv', 'units': 'metric', 'lang': 'uk', 'APPID': "WeatherToken"})
+                           params={'q': 'Kharkiv', 'units': 'metric', 'lang': 'uk', 'APPID': os.getenv("WeatherToken")})
         data = res.json()
         for i in data['list']:
             date = i['dt_txt']
@@ -19,17 +19,17 @@ def weather(update, context):
         context.bot.send_message(update.message.chat_id, 'Погода дала сбой, но я все равно с тобой')
 
 def CurrentWeather(update, context, status=True):
-   # try:
-    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                       params={'q': 'Kharkiv', 'units': 'metric', 'lang': 'uk', 'APPID': "WeatherToken"})
-    data = res.json()
-    description_weather = 'Стан погоди, поки ти сидиш вдома: ' + data['weather'][0]['description']
-    temp = 'Температура твого міста: ' + str(data['main']['temp'])
-    wind = 'Швидкість вітру: ' + str(data['wind']['speed']) + 'м/с'
-    text = description_weather + '. ' + temp + '. \n' + wind
-    if status == True:
-        context.bot.send_message(update.message.chat.id, text)
-    else:
-        return text
-    #except Exception:
-     #   context.bot.send_message(update.message.chat_id, "Та що таке, ти створюєш одні проблеми для мене")
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                           params={'q': 'Kharkiv', 'units': 'metric', 'lang': 'uk', 'APPID': os.getenv("WeatherToken")})
+        data = res.json()
+        description_weather = 'Стан погоди, поки ти сидиш вдома: ' + data['weather'][0]['description']
+        temp = 'Температура твого міста: ' + str(data['main']['temp'])
+        wind = 'Швидкість вітру: ' + str(data['wind']['speed']) + 'м/с'
+        text = description_weather + '. ' + temp + '. \n' + wind
+        if status == True:
+            context.bot.send_message(update.message.chat.id, text)
+        else:
+            return text
+    except Exception:
+        context.bot.send_message(update.message.chat_id, "Та що таке, ти створюєш одні проблеми для мене")
