b66e9b090e658eee2f04b97075c4d880c10dd76f
satiriorn <satiriorn@gmail.com>
2020-09-19 23:51:12 +0300

    add database

---
+++ DB.py
@@ -0,0 +1,29 @@
+import mysql.connector,os
+
+class DataBase:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host=os.getenv("HOST"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+            database="heroku_c93f6b06b535bb4"
+        )
+        self.cursor = self.db.cursor()
+
+    def GetCursor(self):
+        return self.cursor
+    def Commit(self):
+        self.db.commit()
+
+    def CheckUser(self, first_name, username, chat_id):
+        sql = "SELECT count(*) FROM heroku_c93f6b06b535bb4.user WHERE Name = '%s'" % first_name
+        self.cursor.execute(sql)
+        for x in self.cursor:
+            if int(x[0]) == 0:
+                DataBase.Insert(self,first_name,username, chat_id)
+
+    def Insert(self, first_name, username, chat_id):
+        s = "INSERT INTO heroku_c93f6b06b535bb4.user(Name,Username,chatID) VALUES(%s, %s, %s)"
+        val = (first_name,  username, chat_id)
+        self.cursor.execute(s, val)
+        self.Commit()
