1826851497a993816354e91eb8761f336f818345
satiriorn <satiriorn@gmail.com>
2020-10-01 11:50:13 +0300

    stable version of the bot in different languages

--- help.py
+++ help.py
@@ -1,4 +1,5 @@
 import Keyboard, badge, DB
+
 def help(update, context):
     answer = DB.DataBase.GetJsonLanguageBot(badge.DB, update.message.from_user.first_name)
     context.bot.send_message(update.message.chat_id,answer["8"] ,reply_markup = Keyboard.InitKeyboard(badge.MainKeyboard))
--- text.py
+++ text.py
@@ -1,25 +1,25 @@
-import DogAndCat, weather, badge, CreateVoice, UkrainianGame, Meme, AddMusic, Translate, Setting
+import DogAndCat, weather, badge, CreateVoice, UkrainianGame, Meme, AddMusic, Translate
 
 def text(update,context):
-    #try:
-    if badge.CommandVoice == True:
-        CreateVoice.voice(update, context)
+    try:
+        if badge.CommandVoice == True:
+            CreateVoice.voice(update, context)
+            badge.CommandVoice = False
+        elif badge.CommandMusic == True:
+            AddMusic.single_download(update, context)
+        elif badge.CommandTranslate == True:
+            Translate.translate(update, context)
+        elif "погода" == update.message.text.lower():
+            weather.CurrentWeather(update, context)
+        elif update.message.text.lower() == "котик":
+            DogAndCat.Cat_photo(update, context)
+        elif "мем" == update.message.text.lower():
+            Meme.Get_meme(update, context)
+        elif '?' in update.message.text.lower():
+            UkrainianGame.question(update, context)
+    except Exception:
         badge.CommandVoice = False
-    elif badge.CommandMusic == True:
-        AddMusic.single_download(update, context)
-    elif badge.CommandTranslate == True:
-        Translate.translate(update, context)
-    elif 'погода' == update.message.text.lower():
-        weather.CurrentWeather(update, context)
-    elif update.message.text.lower() == 'котик':
-        DogAndCat.Cat_photo(update, context)
-    elif 'мем' == update.message.text.lower():
-        Meme.Get_meme(update, context)
-    elif '?' in update.message.text.lower():
-        UkrainianGame.question(update, context)
-    #except Exception:
-    #    badge.CommandVoice = False
-    #    badge.CommandMusic = False
-    #    badge.CommandTranslate = False
-    #    badge.CommandSettingTranslate = False
-    #    context.bot.send_message(update.message.chat.id, 'Я створив тисячи відповедей задля того, щоб тобі не відповідати.')
+        badge.CommandMusic = False
+        badge.CommandTranslate = False
+        badge.CommandSettingTranslate = False
+        context.bot.send_message(update.message.chat.id, 'Я створив тисячи відповедей задля того, щоб тобі не відповідати.')
