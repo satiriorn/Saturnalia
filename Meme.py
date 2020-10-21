2a15b81461259607a1f9b1ed9226749f6c9c7dd7
satiriorn <satiriorn@gmail.com>
2020-10-21 16:30:57 +0300

    refactoring

--- Meme.py
+++ Meme.py
@@ -19,11 +19,11 @@ def CountMem(update, context):
 def MoreMeme(update, context):
     value = {"0":"0","1":"900","2":"1800", "3":"3600", "4":"7200"}
     if value[str(update.callback_query.data)] != "0":
-        if (update.callback_query.message.chat.type=='group'or update.callback_query.message.chat.type=='supergroup'):
-            badge.job.run_repeating(MemeChatGroup, interval=int(value[str(update.callback_query.data)]), first=0)
+        badge.job.run_repeating(MemeChatGroup, interval=int(value[str(update.callback_query.data)]), first=0, context=update.callback_query.message.chat_id)
     else:
         badge.job.schedule_removal()
     context.bot.edit_message_text(chat_id=update.callback_query.message.chat_id, text=badge.CountMeme[int(update.callback_query.data)], message_id=update.callback_query.message.message_id)
     badge.MemeChange = False
+
 def MemeChatGroup(context: telegram.ext.CallbackContext):
-    context.bot.send_photo('-1001477418565', Link())
+    context.bot.send_photo(context.job.context, Link())
