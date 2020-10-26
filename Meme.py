e26eeec0f0a312a9ae690e61f9a6d1fa09e97eee
satiriorn <satiriorn@gmail.com>
2020-10-26 20:24:49 +0200

    refactoring

--- Meme.py
+++ Meme.py
@@ -4,6 +4,7 @@ def Get_meme(update, context):
     answer = DB.DataBase.GetJsonLanguageBot(badge.DB, update.message.from_user.first_name)
     try:
         Url.Photo(Url.get_url('https://meme-api.herokuapp.com/gimme'), update, context)
+        print(badge.job.jobs())
     except Exception:
         context.bot.send_message(update.message.chat_id, answer["3"])
 
@@ -15,11 +16,19 @@ def CountMem(update, context):
 
 def MoreMeme(update, context):
     value = {"0":"0","1":"900","2":"1800", "3":"3600", "4":"7200"}
+    chat_id = update.callback_query.message.chat_id
     if value[str(update.callback_query.data)] != "0":
-        badge.job.run_repeating(MemeChatGroup, interval=int(value[str(update.callback_query.data)]), first=0, context=update.callback_query.message.chat_id)
+        if str(chat_id) in badge.jobchat.keys():
+            badge.jobchat[str(chat_id)].schedule_removal()
+            badge.jobchat[str(chat_id)] = badge.job.run_repeating(MemeChatGroup, interval=int(value[str(update.callback_query.data)]), first=0,
+                                    context=chat_id)
+        else:
+            badge.jobchat[str(chat_id)] = badge.job.run_repeating(MemeChatGroup, interval=int(value[str(update.callback_query.data)]), first=0,
+                                context=chat_id)
     else:
-        badge.job.schedule_removal()
-    context.bot.edit_message_text(chat_id=update.callback_query.message.chat_id, text=badge.CountMeme[int(update.callback_query.data)], message_id=update.callback_query.message.message_id)
+        badge.jobchat[str(chat_id)].schedule_removal()
+        badge.jobchat.pop(str(chat_id))
+    context.bot.edit_message_text(chat_id=chat_id, text=badge.CountMeme[int(update.callback_query.data)], message_id=update.callback_query.message.message_id)
     badge.MemeChange = False
 
 def MemeChatGroup(context: telegram.ext.CallbackContext):
