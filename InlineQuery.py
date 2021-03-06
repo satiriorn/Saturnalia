95d8d206e0a3799162a9cff7205be4ad259571b1
Satiriorn <satiriorn@gmail.com>
2021-03-06 22:15:23 +0200

    v1.2.9 InlineQuery

--- InlineQuery.py
+++ InlineQuery.py
@@ -1,52 +1,36 @@
-"""
 from uuid import uuid4
-import DogAndCat,Evtuh, weather, CreateVoice, Meme
 from telegram import InlineQueryResultArticle, InputMediaPhoto, ParseMode, InputTextMessageContent, \
     InlineQueryResultCachedVoice, \
     InlineQueryResultPhoto, InputMessageContent, InlineQueryResultCachedPhoto
 from telegram.utils.helpers import escape_markdown
 
-def inlinequery(update, context):
-    try:
+class Inline:
+    _mafina = None
+    def __init__(self, M):
+        Inline._mafina = M
+
+    @classmethod
+    def inlinequery(self, update, context):
+        print(update)
         query = update.inline_query.query
-        photo = DogAndCat.get_url_dog()
         result = []
-        if len(str(query))!= 0:
-            result.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Word of God",
-                description='Цитати Євтушенка',
-                thumb_url=photo,
-                input_message_content=InputTextMessageContent(
-                    Evtuh.Evtuh(context, query, False)),
-                parse_mode=ParseMode.MARKDOWN))
+        if len(str(query))> 0:
+            res = self._mafina._youtube.YoutubeSearch(query)
+            for x in range(len(res)):
+                title = res[x]['title']
+                print(title)
+                description = res[x]['channel']
+                url = res[x]['url_suffix']
+                print(description)
+                result.append(InlineQueryResultArticle(
+                    id=x,
+                    title=str(title),
+                    description=str(description),
+                    input_message_content=InputTextMessageContent(str(url)),
+                    parse_mode=ParseMode.MARKDOWN))
+            print(result[0])
+        """
 
-            result.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Current Weather",
-                thumb_url=photo,
-                description='Погода',
-                input_message_content=InputTextMessageContent(
-                    weather.CurrentWeather(context, update, False)),
-                parse_mode=ParseMode.MARKDOWN))
-
-            result.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Bold",
-                description='Курсивчіку мені',
-                thumb_url=photo,
-                input_message_content=InputTextMessageContent(
-                    "_{}_".format(escape_markdown(query)),
-                    parse_mode=ParseMode.MARKDOWN)))
-
-            result.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Caps",
-                thumb_url=photo,
-                description='Увеличу твой тексток',
-                input_message_content=InputTextMessageContent(
-                    query.upper()),
-                parse_mode=ParseMode.MARKDOWN))
         else:
             result.append(InlineQueryResultPhoto(
                 id=uuid4(),
@@ -71,9 +55,6 @@ def inlinequery(update, context):
                 photo_url=Meme.Link(),
                 thumb_url=photo,
                 parse_mode=ParseMode.MARKDOWN))
-
+        """
         update.inline_query.answer(result)
-    except:
-        u = 'update.inline_query.from.id'
-        #bot.send_message(u, 'Вибач, мої мікросхеми згоріли, пиши пізніше...')
-"""
+
