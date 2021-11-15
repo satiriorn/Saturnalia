fdaa5d382abbf3763b1cf767e01bf169f07a9e7e
satiriorn <45328793+satiriorn@users.noreply.github.com>
2021-11-15 17:30:30 +0200

    v1.7.4 add buy and sell

--- Hunter_of_BinanceAnnouncements.py
+++ Hunter_of_BinanceAnnouncements.py
@@ -1,11 +1,11 @@
-import telegram.ext, os, re, math, Thread, requests
+import re, math, Thread, requests, os
 import time
 from decimal import Decimal as D
 from gate_api import ApiClient, Configuration, Order, SpotApi
 
 class Hunter:
 	_instance, _mafina = None, None
-	NewListing = ""
+	Purchased = False
 	chat_id = "-506807179"
 	Currency = "_USDT"
 	LastListing = ""
@@ -22,11 +22,15 @@ class Hunter:
 			if Hunter.LastListing == "":
 				Hunter.LastListing = hunter._DB.GetUsername(self.chat_id)
 			while True:
-				time.sleep(2)
+				time.sleep(3)
 				try:
-					Thread.Thread(self.CheckListing, (hunter, bot))
+					if self.Purchased:
+						Thread.Thread(self.SellCrypto, ())
+						self.CheckListing(hunter, bot)
+					else:
+						Thread.Thread(self.CheckListing, (hunter, bot))
 				except Exception:
-					time.sleep(1)
+					time.sleep(2)
 					Thread.Thread(self.HunterListing(), ())
 		except Exception:
 			time.sleep(2)
@@ -36,41 +40,60 @@ class Hunter:
 		try:
 			if self.chat_id in hunter.UseCommand.keys():
 				if hunter.UseCommand[self.chat_id] == "NewListing":
-					bot.send_messages(self.chat_id, Hunter.NewListing)
+					bot.send_message(self.chat_id, self.LastListing)
+					return 0
 			latest_announcement = requests.get(
 				"https://www.binance.com/bapi/composite/v1/public/cms/article/catalog/list/query?catalogId=48&pageNo=1&pageSize=15&rnd=" + str(
 					time.time())).json()
-			UpdateListing = latest_announcement['data']['articles'][0]['title'] + " " + time.strftime("%H:%M:%S",
-																									  time.localtime())
-			print(UpdateListing)
+			UpdateListing = latest_announcement['data']['articles'][0]['title']
+			print(UpdateListing+ " " + time.strftime("%H:%M:%S", time.localtime()))
 			if "binance will list" in UpdateListing.lower():
 				if self.LastListing != UpdateListing:
+					print("Check")
+					self.BuyingNewCrypto(UpdateListing)
 					Hunter._instance._mafina.UseCommand[self.chat_id] = "NewListing"
-					hunter._DB.UpdateListing(self.chat_id, UpdateListing)
 					self.LastListing = UpdateListing
-					bot.send_message(self.chat_id, Hunter.NewListing)
+					bot.send_message(self.chat_id, self.LastListing)
+					hunter._DB.UpdateListing(self.chat_id, UpdateListing+" " + time.strftime("%H:%M:%S", time.localtime()))
+
 		except Exception:
 			time.sleep(2)
-			self.CheckListing(hunter, bot)
-	"""
 
-	@staticmethod
-	def BuyingNewCrypto():
-		Name_crypto = re.search(r"\(([A-Za-z0-9_]+)\)", Hunter.NewListing).group(1)
-		currency_pair = Name_crypto + "_USDT"
-		config = Configuration(key=os.getenv('Gate_key'), secret=os.getenv('Gate_secret'))
-		spot_api = SpotApi(ApiClient(config))
-		tickers = spot_api.list_tickers(currency_pair=currency_pair)
-		last_price = float(tickers[0].last)
-		accounts = spot_api.list_spot_accounts(currency="USDT")
-		available = float(D(accounts[0].available))
-		price_buying = last_price + last_price / 100 * (10)
-		amount_order = available/price_buying
+
+	def BuyingNewCrypto(self, UpdateListing):
+		self.Name_crypto = re.search(r"\(([A-Za-z0-9_]+)\)", UpdateListing).group(1)
+		self.currency_pair = self.Name_crypto + "_USDT"
+		self.config = Configuration(key=os.getenv('Gate_key'), secret=os.getenv('Gate_secret'))
+		self.spot_api = SpotApi(ApiClient(self.config))
+		self.tickers = self.spot_api.list_tickers(currency_pair=self.currency_pair)
+		last_price = float(self.tickers[0].last)
+		self.accounts = self.spot_api.list_spot_accounts(currency="USDT")
+		available = float(D(self.accounts[0].available))
+		self.price_buying = last_price + last_price / 100 * (10)
+		amount_order = available / self.price_buying
 		amount_order = math.floor(amount_order * 10) / 10
 		print(amount_order)
 		print(available)
-		print(str(price_buying))
-		order = Order(amount=str(amount_order), price=str(price_buying), side='buy', currency_pair=currency_pair)
-		created = spot_api.create_order(order)
-		print(created.status)
-	"""
+		print(str(self.price_buying))
+		order = Order(amount=str(amount_order), price=str(self.price_buying), side='buy', currency_pair=self.currency_pair)
+		self.created_buy = self.spot_api.create_order(order)
+		print(self.created_buy.status)
+		if str(self.created_buy.status) == "closed":
+			self.Purchased = True
+
+
+
+	def SellCrypto(self):
+		sell_price = self.price_buying + self.price_buying / 100 * (80)
+		if str(self.created_buy.status) == "closed":
+			last_price = float(self.tickers[0].last)
+			print(last_price)
+			print(sell_price)
+			if sell_price <= float(self.tickers[0].last):
+				available = self.spot_api.list_spot_accounts(currency=self.Name_crypto)[0].available
+				last_price = last_price + last_price / 100 * (-5)
+				order = Order(amount=str(available), price=str(last_price), side='sell',
+							  currency_pair=self.currency_pair)
+				self.created_sell = self.spot_api.create_order(order)
+				if (self.created_sell.status == "closed"):
+					self.Purchased = False
